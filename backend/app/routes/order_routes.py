dfdb06d393b02b70f491fc6ea927c0ed6ab1c9d7
piyan <wong920907@gmail.com>
2024-05-10 14:56:49 +0800

    order_routes ok

--- backend/app/routes/order_routes.py
+++ backend/app/routes/order_routes.py
@@ -4,16 +4,15 @@ from ..database import execute_query
 order_bp = Blueprint('order', __name__)
 
 # 提交一筆訂單
-@order_bp.route("/api/<string:en_store_name>/order", methods=["POST"])
-def create_order(en_store_name):
+@order_bp.route("/api/<string:store_id>/order", methods=["POST"])
+def create_order(store_id):
     data = request.json
-    order_id = data.get('order_id')
     userid = data.get('userid')
     group_buying_id = data.get('group_buying_id')
     quantity = data.get('quantity')
 
-    query = "INSERT INTO `Order` (order_id, userid, group_buying_id, quantity) VALUES (%s, %s, %s, %s)"
-    result = execute_query(query, (order_id, userid, group_buying_id, quantity))
+    query = "INSERT INTO `Order` (userid, group_buying_id, quantity) VALUES (%s, %s, %s)"
+    result = execute_query(query, (userid, group_buying_id, quantity))
 
     if result:
         return jsonify({'message': 'Order created successfully'}), 200
@@ -21,10 +20,16 @@ def create_order(en_store_name):
     return jsonify({'error': 'Failed to create order'}), 500
 
 # 查詢一筆訂單
-@order_bp.route("/api/<string:en_store_name>/order/<int:order_id>", methods=["GET"])
-def get_order_by_order_id(en_store_name, order_id):
-    query = "SELECT * FROM `Order` WHERE order_id = %s"
-    order = execute_query(query, (order_id,))
+@order_bp.route("/api/<string:store_id>/order/<int:order_id>", methods=["GET"])
+def get_order_by_order_id(store_id, order_id):
+    query = """
+                SELECT `Order`.*
+                FROM `Order`
+                INNER JOIN Group_buying_product ON `Order`.group_buying_id = Group_buying_product.group_buying_id
+                INNER JOIN Product ON Group_buying_product.product_id = Product.product_id
+                WHERE `Order`.order_id = %s AND Product.store_id = %s;
+            """
+    order = execute_query(query, (order_id, store_id))
 
     if order:
         order_dict = {
@@ -36,19 +41,20 @@ def get_order_by_order_id(en_store_name, order_id):
         }
         return jsonify(order_dict), 200
     
-    return jsonify({'message':'Fail to get all orders by orderid'}), 404
+    return jsonify({'message':'Fail to get order by orderid'}), 404
 
 # 查詢一名客戶所有清單
-@order_bp.route("/api/<string:en_store_name>/order/<string:userid>", methods=["GET"])
-def get_all_orders_by_userid(en_store_name, userid):
+@order_bp.route("/api/<string:store_id>/order/<string:userid>", methods=["GET"])
+def get_all_orders_by_userid(store_id, userid):
     query = """
                 SELECT `Order`.*
                 FROM `Order`
                 INNER JOIN Group_buying_product ON `Order`.group_buying_id = Group_buying_product.group_buying_id
+                INNER JOIN Product ON Group_buying_product.product_id = Product.product_id
                 WHERE `Order`.userid = %s
-                AND Group_buying_product.en_store_name = %s;
+                AND Product.store_id = %s;
             """
-    orders = execute_query(query, (userid, en_store_name), True)
+    orders = execute_query(query, (userid, store_id), True)
     
     data = []
     if orders:
@@ -66,20 +72,25 @@ def get_all_orders_by_userid(en_store_name, userid):
 
     return jsonify({'message' : 'Fail to get all orders by userid'}), 404
 
-@order_bp.route("/api/<string:en_store_name>/order/<string:userid>/<int:status>", methods=["GET"])
-def get_all_orders_by_userid_and_status(en_store_name, userid, status):
-    status = True if status == 1 else False
-
-    # query = "SELECT * FROM `Order` WHERE userid = %s AND receive_status = %s"
+@order_bp.route("/api/<string:store_id>/order/<string:userid>/<int:status>", methods=["GET"])
+def get_all_orders_by_userid_and_status(store_id, userid, status):
+    if status == 0:
+        status = False
+    elif status == 1:
+        status = True
+    else:
+        return jsonify({'message' : 'Invalid status'}), 404
+    
     query = """
                 SELECT `Order`.*
                 FROM `Order`
                 INNER JOIN Group_buying_product ON `Order`.group_buying_id = Group_buying_product.group_buying_id
+                INNER JOIN Product ON Group_buying_product.product_id = Product.product_id
                 WHERE `Order`.userid = %s
-                AND Group_buying_product.en_store_name = %s
+                AND Product.store_id = %s
                 AND receive_status = %s;
             """
-    orders = execute_query(query, (userid, en_store_name, status), True)
+    orders = execute_query(query, (userid, store_id, status), True)
 
     data = []
     if orders:
