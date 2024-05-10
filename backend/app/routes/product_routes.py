060dbbc390bc062d9d396d014f05407e390863d0
piyan <wong920907@gmail.com>
2024-05-10 14:25:23 +0800

    product_routes ok

--- backend/app/routes/product_routes.py
+++ backend/app/routes/product_routes.py
@@ -4,12 +4,11 @@ from ..database import execute_query
 product_bp = Blueprint('product', __name__)
 
 # 獲取商家的所有商品列表
-@product_bp.route("/api/<string:en_store_name>/product", methods=["GET"])
-def get_all_products_by_storename(en_store_name):
+@product_bp.route("/api/<string:store_id>/product", methods=["GET"])
+def get_all_products_by_storename(store_id):
     query = """
                 SELECT 
                     GBP.group_buying_id,
-                    GBP.en_store_name,
                     GBP.purchase_quantity,
                     GBP.launch_date,
                     GBP.statement_date,
@@ -19,6 +18,7 @@ def get_all_products_by_storename(en_store_name):
                     GBP.income,
                     GBP.cost,
                     P.product_id,
+                    P.store_id,
                     P.price,
                     P.product_describe,
                     P.supplier_name,
@@ -29,32 +29,32 @@ def get_all_products_by_storename(en_store_name):
                 INNER JOIN 
                     Product P ON GBP.product_id = P.product_id
                 WHERE
-                    GBP.en_store_name = %s;
+                    P.store_id = %s;
             """
     
-    products = execute_query(query, (en_store_name,), True)
+    products = execute_query(query, (store_id,), True)
 
     data = []
     if products:
         for product in products:
             data.append(
                 {
-                    "group_buying_id" : product[0],
-                    "en_store_name" : product[1],
-                    "purchase_quantity" : product[2],
-                    "launch_date" : product[3],
-                    "statement_date" : product[4],
-                    "arrival_date" : product[5],
-                    "due_days" : product[6],
-                    "inventory" : product[7],
-                    "income" : product[8],
-                    "cost" : product[9],
-                    "product_id" : product[10],
-                    "price" : product[11],
-                    "product_describe" : product[12],
-                    "supplier_name" : product[13],
-                    "product_name" : product[14],
-                    "product_picture" : product[15]
+                    "group_buying_id": product[0],
+                    "purchase_quantity": product[1],
+                    "launch_date": product[2],
+                    "statement_date": product[3],
+                    "arrival_date": product[4],
+                    "due_days": product[5],
+                    "inventory": product[6],
+                    "income": product[7],
+                    "cost": product[8],
+                    "product_id": product[9],
+                    "store_id": product[10],
+                    "price": product[11],
+                    "product_describe": product[12],
+                    "supplier_name": product[13],
+                    "product_name": product[14],
+                    "product_picture": product[15]
                 }
             )
         return jsonify(data), 200
@@ -62,12 +62,11 @@ def get_all_products_by_storename(en_store_name):
     return jsonify({"message": "Fail to get all products by storename"}), 404
 
 # 獲取一筆團購訂單
-@product_bp.route("/api/<string:en_store_name>/product/<int:group_buying_id>", methods=["GET"])
-def get_product_by_group_buying_id(en_store_name, group_buying_id):
+@product_bp.route("/api/<string:store_id>/product/<int:group_buying_id>", methods=["GET"])
+def get_product_by_group_buying_id(store_id, group_buying_id):
     query = """
                 SELECT 
                     GBP.group_buying_id,
-                    GBP.en_store_name,
                     GBP.purchase_quantity,
                     GBP.launch_date,
                     GBP.statement_date,
@@ -77,6 +76,7 @@ def get_product_by_group_buying_id(en_store_name, group_buying_id):
                     GBP.income,
                     GBP.cost,
                     P.product_id,
+                    P.store_id,
                     P.price,
                     P.product_describe,
                     P.supplier_name,
@@ -87,32 +87,32 @@ def get_product_by_group_buying_id(en_store_name, group_buying_id):
                 INNER JOIN 
                     Product P ON GBP.product_id = P.product_id
                 WHERE
-                    GBP.en_store_name = %s
+                    P.store_id = %s
                 AND
                     GBP.group_buying_id = %s;
             """
 
-    product = execute_query(query, (en_store_name, group_buying_id))
+    product = execute_query(query, (store_id, group_buying_id))
 
     if product:
-        product_dict = {
-                            "group_buying_id" : product[0],
-                            "en_store_name" : product[1],
-                            "purchase_quantity" : product[2],
-                            "launch_date" : product[3],
-                            "statement_date" : product[4],
-                            "arrival_date" : product[5],
-                            "due_days" : product[6],
-                            "inventory" : product[7],
-                            "income" : product[8],
-                            "cost" : product[9],
-                            "product_id" : product[10],
-                            "price" : product[11],
-                            "product_describe" : product[12],
-                            "supplier_name" : product[13],
-                            "product_name" : product[14],
-                            "product_picture" : product[15]
-                        }
+        product_dict =  {
+                    "group_buying_id": product[0],
+                    "purchase_quantity": product[1],
+                    "launch_date": product[2],
+                    "statement_date": product[3],
+                    "arrival_date": product[4],
+                    "due_days": product[5],
+                    "inventory": product[6],
+                    "income": product[7],
+                    "cost": product[8],
+                    "product_id": product[9],
+                    "store_id": product[10],
+                    "price": product[11],
+                    "product_describe": product[12],
+                    "supplier_name": product[13],
+                    "product_name": product[14],
+                    "product_picture": product[15]
+                }
         return jsonify(product_dict), 200
 
-    return jsonify({"message": "Fail to get product by group buying id"}), 404
+    return jsonify({"message": "Fail to get product by groupbuying id"}), 404
