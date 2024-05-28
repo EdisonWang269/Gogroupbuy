67e68db2ef76b12c33b5c97f875402590c88c467
Rhapsody027 <101376557+Rhapsody027@users.noreply.github.com>
2024-05-29 06:37:03 +0800

    add format

---
+++ frontend/eslint.config.js
@@ -0,0 +1,42 @@
+import pluginVue from "eslint-plugin-vue";
+export default [
+	...pluginVue.configs["flat/recommended"],
+	{
+		rules: {
+			"vue/padding-lines-in-component-definition": [
+				"error",
+				{
+					betweenOptions: "always" | "never",
+
+					withinOption:
+						{
+							props:
+								{
+									betweenItems: "always" | "never" | "ignore",
+									withinEach: "always" | "never" | "ignore",
+								} |
+								"always" |
+								"never" |
+								"ignore", // shortcut to set both
+
+							data:
+								{
+									betweenItems: "always" | "never" | "ignore",
+									withinEach: "always" | "never" | "ignore",
+								} |
+								"always" |
+								"never" |
+								"ignore", // shortcut to set both
+
+							// ... all options
+						} |
+						"always" |
+						"never" |
+						"ignore",
+
+					groupSingleLineProperties: true | false,
+				},
+			],
+		},
+	},
+];
