75f49ad18ca84d3147719825b04333e831120be7
ChenHsuanYu <sharonchen82228@gmail.com>
2024-06-02 18:00:49 +0800

    部署嘗試

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -7,6 +7,22 @@ module.exports = {
       }
     }
   },
+    pages: {
+      index: {
+        entry: 'src/views/UserAll.vue',
+        template: 'public/index.html',
+        filename: 'homePage.html',
+        title: 'Home Page',
+        chunks: ['chunk-vendors', 'chunk-common', 'index'],
+      },
+      about: {
+        entry: 'src/views/UserHistoryPage.vue',
+        template: 'public/index.html',
+        filename: 'history.html',
+        title: 'History Page',
+        chunks: ['chunk-vendors', 'chunk-common', 'about'],
+      },
+    },
 
   publicPath: "/Gogroupbuy/"
 };
