f7fa72b4daa7bb36524fcd391726d695e5b71379
Rhapsody027 <101376557+Rhapsody027@users.noreply.github.com>
2024-06-06 02:31:41 +0800

    fix api

--- backend/app/routes/product_routes.py
+++ backend/app/routes/product_routes.py
@@ -83,9 +83,9 @@ def get_all_products_by_storeid():
     if products:
         for product in products:
             # 將LONGBLOB數據轉換為Base64字符串
-            product_picture_base64 = (
-                base64.b64encode(product[7]) if product[7] else None
-            )
+            # product_picture_base64 = (
+            #     base64.b64encode(product[7]) if product[7] else None
+            # )
 
             # 王:
             # if product[7]:
