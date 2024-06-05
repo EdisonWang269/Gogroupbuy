540ac170d0fec175d749d14727820d6d4e6e9e4f
Rhapsody027 <101376557+Rhapsody027@users.noreply.github.com>
2024-06-05 22:26:38 +0800

    更新vue router版本至最新

--- frontend/src/views/UserAll.vue
+++ frontend/src/views/UserAll.vue
@@ -1,25 +1,25 @@
 <template>
-    <div class="content">
-        <router-view></router-view>
-    </div>
-    <nav-bar class="navBar"/>
+  <div class="content">
+    <router-view></router-view>
+  </div>
+  <nav-bar class="navBar" />
 </template>
 
 <script>
-import NavBar from '@/components/NavBar.vue';
-export default {
-    components:{
-        NavBar,
-    }
-}
+  import NavBar from "@/components/NavBar.vue";
+  export default {
+    components: {
+      NavBar,
+    },
+  };
 </script>
 
 <style scoped>
-.content{
+  .content {
     margin-bottom: 5%;
-}
-.navBar{
+  }
+  .navBar {
     position: fixed;
-    bottom:0;
-}
+    bottom: 0;
+  }
 </style>
