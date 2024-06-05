d599a6e9f48c0ff6d71b22d23def0c958240d097
piyan <wong920907@gmail.com>
2024-06-05 22:32:00 +0800

    try pic

--- backend/app/routes/product_routes.py
+++ backend/app/routes/product_routes.py
@@ -83,9 +83,7 @@ def get_all_products_by_storeid():
     if products:
         for product in products:
             # 將LONGBLOB數據轉換為Base64字符串
-            product_picture_base64 = base64.b64encode(product[7])
-            product_picture_base64 = product_picture_base64.decode("utf-8")
-            
+            product_picture_base64 = base64.b64encode(product[7]).decode('utf-8') if product[7] else None
             data.append(
                 {
                     "group_buying_id": product[0],
