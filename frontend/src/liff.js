e7a6ddb949e1115ed578f2dba9ee531a34b0822b
Rhapsody027 <101376557+Rhapsody027@users.noreply.github.com>
2024-06-09 21:26:27 +0800

    change liff id

--- frontend/src/liff.js
+++ frontend/src/liff.js
@@ -3,41 +3,39 @@ import store from "./store";
 
 export const initializeLiff = async () => {
   try {
-    await liff.init({ liffId: '2004368945-ZXAjYNkb' })
-      .then(() => {
-        if (!liff.isLoggedIn()) {
-          console.log("not loggedIn");
-          liff.login();
-        } else {
-          console.log("loggedIn");
-          liff.getProfile().then(async (profile) => {
-            console.log(profile.displayName);
-            console.log(profile.userId);
-            console.log(profile.pictureUrl);
-            store.commit("user/setUserImg", profile.pictureUrl);
-            store.commit("user/setUserID", profile.userId);
-            store.commit("user/setUserName", profile.displayName);
-            const response = await fetch(`/api/user`, {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({
-                store_id: store.state.manager.storeID,
-                userid: store.state.user.userID,
-              }),
-            });
-            // You might want to handle the response here
-            const responseData = await response.json();
-            console.log(responseData);
+    await liff.init({ liffId: "2005547450-kezMVvEn" }).then(() => {
+      if (!liff.isLoggedIn()) {
+        console.log("not loggedIn");
+        liff.login();
+      } else {
+        console.log("loggedIn");
+        liff.getProfile().then(async (profile) => {
+          console.log(profile.displayName);
+          console.log(profile.userId);
+          console.log(profile.pictureUrl);
+          store.commit("user/setUserImg", profile.pictureUrl);
+          store.commit("user/setUserID", profile.userId);
+          store.commit("user/setUserName", profile.displayName);
+          const response = await fetch(`/api/user`, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              store_id: store.state.manager.storeID,
+              userid: store.state.user.userID,
+            }),
           });
-        }
-      });
-    console.log('LIFF initialized');
+          // You might want to handle the response here
+          const responseData = await response.json();
+          console.log(responseData);
+        });
+      }
+    });
+    console.log("LIFF initialized");
   } catch (error) {
     console.error("LIFF initialization failed", error);
   }
 };
-
 
 export default liff;
